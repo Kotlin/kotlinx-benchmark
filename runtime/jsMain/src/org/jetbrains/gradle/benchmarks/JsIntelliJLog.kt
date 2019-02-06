aab3b611ef4f0d419916042c0afd89ae7dda852a
Ilya Ryzhenkov <orangy@jetbrains.com>
2019-02-06 14:44:59 +0300

    Fix JS utf8 encoding

--- runtime/jsMain/src/org/jetbrains/gradle/benchmarks/JsIntelliJLog.kt
+++ runtime/jsMain/src/org/jetbrains/gradle/benchmarks/JsIntelliJLog.kt
@@ -1,4 +1,9 @@
 package org.jetbrains.gradle.benchmarks
 
 // TODO: Use UTF8 from kotlinx.io, it's okay while we don't have any non-ascii chars
-actual fun String.toByteArrayUtf8(): ByteArray = map { it.toByte() }.toByteArray()
+@Suppress("UNUSED_VARIABLE") // KT-23633
+actual fun String.toByteArrayUtf8(): ByteArray {
+    val s = this
+    val block = js("unescape(encodeURIComponent(s))") // contains only chars that fit to byte
+    return (block as String).toList().map { it.toByte() }.toByteArray()
+}
