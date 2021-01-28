1129592874c1d7062b228f361af4f1e8f0ba26c0
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2021-01-28 16:50:08 +0300

    Rewrote deprecated things and add consistency with compiler flags

--- runtime/nativeMain/src/kotlinx/benchmark/NativeBlackhole.kt
+++ runtime/nativeMain/src/kotlinx/benchmark/NativeBlackhole.kt
@@ -1,13 +1,37 @@
 package kotlinx.benchmark
 
 actual class Blackhole {
-    actual fun consume(obj: Any?) {}
-    actual fun consume(bool: Boolean) {}
-    actual fun consume(c: Char) {}
-    actual fun consume(b: Byte) {}
-    actual fun consume(s: Short) {}
-    actual fun consume(i: Int) {}
-    actual fun consume(l: Long) {}
-    actual fun consume(f: Float) {}
-    actual fun consume(d: Double) {}
+    @ThreadLocal
+    companion object {
+        var consumer = 0
+    }
+    actual fun consume(obj: Any?) {
+        // hashCode now is implemented as taking address of object, so it's suitable now.
+        // If implementation is changed `Blackhole` should be reimplemented.
+        consumer += obj.hashCode()
+    }
+    actual fun consume(bool: Boolean) {
+        consumer += bool.hashCode()
+    }
+    actual fun consume(c: Char) {
+        consumer += c.hashCode()
+    }
+    actual fun consume(b: Byte) {
+        consumer += b.hashCode()
+    }
+    actual fun consume(s: Short) {
+        consumer += s.hashCode()
+    }
+    actual fun consume(i: Int) {
+        consumer += i.hashCode()
+    }
+    actual fun consume(l: Long) {
+        consumer += l.hashCode()
+    }
+    actual fun consume(f: Float) {
+        consumer += f.hashCode()
+    }
+    actual fun consume(d: Double) {
+        consumer += d.hashCode()
+    }
 }
