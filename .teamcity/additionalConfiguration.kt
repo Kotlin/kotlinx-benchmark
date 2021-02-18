9dbbc2aa147cafb84f771732f22542bbe0979877
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2021-02-19 01:59:01 +0300

    Add deploy plugin configuration

--- .teamcity/additionalConfiguration.kt
+++ .teamcity/additionalConfiguration.kt
@@ -3,14 +3,19 @@
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildTypeSettings
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.GradleBuildStep
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 
 fun Project.additionalConfiguration() {
     platforms.forEach { platform ->
         val gradleBuild = knownBuilds.buildOn(platform).steps.items.single() as GradleBuildStep
         gradleBuild.tasks += " " + fastBenchmarkTasks(platform)
     }
+
+    deployPlugin()
 }
 
 fun fastBenchmarkTasks(platform: Platform): String {
@@ -18,3 +23,38 @@ fun fastBenchmarkTasks(platform: Platform): String {
         "js", "jvm", platform.nativeTaskPrefix()
     ).joinToString(separator = " ", transform = { "${it}FastBenchmark" })
 }
+
+const val gradlePublishKey = "gradle.publish.key"
+const val gradlePublishSecret = "gradle.publish.secret"
+
+const val DEPLOY_PUBLISH_PLUGIN_ID = "Deploy_Publish_Plugin"
+
+fun Project.deployPlugin() = BuildType {
+    id(DEPLOY_PUBLISH_PLUGIN_ID)
+    this.name = "Deploy (Publish Plugin)"
+    commonConfigure()
+
+    requirements {
+        // Require Linux for publishPlugins
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+
+    dependsOnSnapshot(this@deployPlugin.knownBuilds.buildAll)
+    buildNumberPattern = this@deployPlugin.knownBuilds.buildVersion.depParamRefs.buildNumber.ref
+
+    type = BuildTypeSettings.Type.DEPLOYMENT
+    enablePersonalBuilds = false
+    maxRunningBuilds = 1
+
+    steps {
+        gradle {
+            name = "Publish Plugin"
+            jdkHome = "%env.$jdk%"
+            jvmArgs = "-Xmx1g"
+            gradleParams = "--info --stacktrace -P$gradlePublishKey=%$gradlePublishKey% -P$gradlePublishSecret=%$gradlePublishSecret%"
+            tasks = "clean :plugin:publishPlugins"
+            buildFile = ""
+            gradleWrapperPath = ""
+        }
+    }
+}.also { buildType(it) }
