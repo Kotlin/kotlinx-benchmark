272bfb14fb4fcdccadcd972252ea4e18664096d9
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2022-01-21 17:51:47 +0300

    Move quoted parameter value test from integration test to example project

--- examples/kotlin-multiplatform/src/commonMain/kotlin/ParamBenchmark.kt
+++ examples/kotlin-multiplatform/src/commonMain/kotlin/ParamBenchmark.kt
@@ -8,27 +8,30 @@ import kotlin.math.*
 @OutputTimeUnit(BenchmarkTimeUnit.MILLISECONDS)
 @BenchmarkMode(Mode.Throughput)
 class ParamBenchmark {
-    
+
     @Param("1", "2")
     var data = 0
 
     @Param("1", "2")
     var value = 0
-    
-    private lateinit var text : String 
-    
-    @Setup
-    fun setUp() {
-        text = "Hello!"
-    }
-    
+
+    @Param("""a "string" with quotes""")
+    var text: String = ""
+
     @Benchmark
     fun mathBenchmark(): Double {
         return log(sqrt(data.toDouble()) * data, 2.0)
     }
-    
+
     @Benchmark
     fun otherBenchmark(): Int {
         return data + data
+    }
+
+    @Benchmark
+    fun textContentCheck(): String {
+        check(text.length == 22)
+        check(text.count { it == '\"' } == 2)
+        return text
     }
 }
