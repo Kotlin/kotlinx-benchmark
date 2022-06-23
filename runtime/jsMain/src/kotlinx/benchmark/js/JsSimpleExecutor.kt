20d20c2608f2b525a7d78f6c7ca17e2b5fd0c093
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-06-23 12:18:05 +0200

    Add deoptimizer to JsCommonExecutor

--- runtime/jsMain/src/kotlinx/benchmark/js/JsSimpleExecutor.kt
+++ runtime/jsMain/src/kotlinx/benchmark/js/JsSimpleExecutor.kt
@@ -1,9 +1,35 @@
 package kotlinx.benchmark.js
 
+import kotlinx.benchmark.BenchmarkConfiguration
+import kotlinx.benchmark.BenchmarkDescriptor
 import kotlinx.benchmark.CommonSuitExecutor
 import kotlinx.benchmark.jsEngineSupport
+import kotlin.math.sin
+
+@JsName("Function")
+private external fun functionCtor(params: String, code: String): (dynamic) -> Long
 
 class JsSimpleExecutor(
     name: String,
     @Suppress("UNUSED_PARAMETER") dummy_args: Array<out String>
-) : CommonSuitExecutor(name, jsEngineSupport.arguments()[0])
+) : CommonSuitExecutor(name, jsEngineSupport.arguments()[0]) {
+
+    private val BenchmarkConfiguration.jsUseBridge: Boolean
+        get() = "true".equals(advanced["jsUseBridge"], ignoreCase = true)
+
+    private fun createJsMeasurerBridge(originalMeasurer: () -> Long): () -> Long {
+        val bridgeObject = object {
+            fun invoke(): Long = originalMeasurer.invoke()
+        }
+        val measurerString = bridgeObject::invoke.toString()
+        val measurerBody = measurerString.substringAfter("{").substringBeforeLast("}")
+        return {
+            functionCtor("\$boundThis", measurerBody)(bridgeObject)
+        }
+    }
+
+    override fun <T> createIterationMeasurer(instance: T, benchmark: BenchmarkDescriptor<T>, configuration: BenchmarkConfiguration): () -> Long {
+        val measurer = super.createIterationMeasurer(instance, benchmark, configuration)
+        return if (configuration.jsUseBridge) createJsMeasurerBridge(measurer) else measurer
+    }
+}
