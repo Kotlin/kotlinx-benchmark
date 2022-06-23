363a322bc3664daa1f8fa8fc940df042014d55cf
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2022-06-23 13:39:23 +0300

    Promote the library version to 0.4.3

--- examples/kotlin-kts/build.gradle.kts
+++ examples/kotlin-kts/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     java
     kotlin("jvm")
     kotlin("plugin.allopen") version "1.7.0"
-    id("org.jetbrains.kotlinx.benchmark") version "0.4.2"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.3"
 }
 
 sourceSets.all {
