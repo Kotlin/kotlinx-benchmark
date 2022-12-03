16980d2cc800fef0ba32dbc4fc8d052265a6f454
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2022-12-03 23:13:20 +0200

    Change import package of KotlinLibraryResolverResultImpl
    It is required because the class was moved to a new package in Kotlin. It was done as part of an effort to "Bring package names in sync with the directory layout".

--- plugin/main/src/kotlinx/benchmark/gradle/KlibResolver.kt
+++ plugin/main/src/kotlinx/benchmark/gradle/KlibResolver.kt
@@ -9,7 +9,7 @@ import org.jetbrains.kotlin.konan.library.*
 import org.jetbrains.kotlin.library.*
 import org.jetbrains.kotlin.library.impl.*
 import org.jetbrains.kotlin.library.metadata.*
-import org.jetbrains.kotlin.library.resolver.impl.*
+import org.jetbrains.kotlin.library.metadata.resolver.impl.*
 import org.jetbrains.kotlin.storage.*
 import org.jetbrains.kotlin.util.*
 import java.io.*
