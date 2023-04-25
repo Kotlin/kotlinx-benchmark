26b7bcdcb2621ac35b944d43b10e764f2fb13bb4
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-04-25 15:01:10 +0200

    Advance Kotlin version to 1.8.21

--- examples/kotlin-kts/build.gradle.kts
+++ examples/kotlin-kts/build.gradle.kts
@@ -8,7 +8,7 @@ import org.jetbrains.kotlin.gradle.tasks.*
 plugins {
     java
     kotlin("jvm")
-    kotlin("plugin.allopen") version "1.7.20"
+    kotlin("plugin.allopen") version "1.8.21"
     id("org.jetbrains.kotlinx.benchmark") version "0.4.7"
 }
 
