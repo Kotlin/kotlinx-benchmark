d539ea4366bcd04feac107bc9f8554c641704831
Youssef Shoaib <canonballt@gmail.com>
2023-04-28 18:08:30 +0100

    Support registering multiplatform source sets as benchmark targets

--- plugin/main/src/kotlinx/benchmark/gradle/BenchmarksExtension.kt
+++ plugin/main/src/kotlinx/benchmark/gradle/BenchmarksExtension.kt
@@ -51,7 +51,9 @@ open class BenchmarksExtension(val project: Project) {
             when {
                 multiplatform != null -> {
                     val target = multiplatform.targets.findByName(name)
-                    when (val compilation = target?.compilations?.findByName(KotlinCompilation.MAIN_COMPILATION_NAME)) {
+                    // We allow the name to be either a target or a source set
+                    when (val compilation = target?.compilations?.findByName(KotlinCompilation.MAIN_COMPILATION_NAME)
+                        ?: multiplatform.targets.flatMap { it.compilations }.find { it.defaultSourceSetName == name }) {
                         null -> {
                             project.logger.warn("Warning: Cannot find a benchmark compilation '$name', ignoring.")
                             BenchmarkTarget(this, name) // ignore
