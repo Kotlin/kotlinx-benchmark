e00e1f94c22f8a3c8ac7c4f2afee98d25b528577
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2023-07-14 00:49:15 +0300

    Do not specify plugin version as it is an included build
    Those dependencies are substituted by Gradle when the plugin `id` or `group:name` is specified correctly.

--- examples/kotlin-kts/build.gradle.kts
+++ examples/kotlin-kts/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     java
     kotlin("jvm")
     kotlin("plugin.allopen") version "1.8.21"
-    id("org.jetbrains.kotlinx.benchmark") version "0.5.0-SNAPSHOT"
+    id("org.jetbrains.kotlinx.benchmark")
 }
 
 sourceSets.all {
