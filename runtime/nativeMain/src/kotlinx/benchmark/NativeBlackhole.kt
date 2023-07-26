0e8acc0ffb52e48198c1401803bae5b855219f63
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2023-07-26 13:13:49 +0200

    Use identify hash code to consume objects

--- runtime/nativeMain/src/kotlinx/benchmark/NativeBlackhole.kt
+++ runtime/nativeMain/src/kotlinx/benchmark/NativeBlackhole.kt
@@ -2,35 +2,13 @@ package kotlinx.benchmark
 
 import kotlinx.cinterop.convert
 import kotlinx.cinterop.toByte
-import kotlin.concurrent.Volatile
-import kotlin.native.ref.WeakReference
-import kotlin.random.Random
 import kotlinx.benchmark.impl.*
+import kotlin.native.identityHashCode
 
-@OptIn(ExperimentalStdlibApi::class)
 actual class Blackhole {
-    /*
-     * For primitive types, DCE may be omitted by creating an artificial use via inline asm.
-     * For objects, there is no cheap way to obtain something that could be passed to a native function with such
-     * inline asm, so instead we're generating a sequence of random numbers and as soon as we get zero (that's highly
-     * unlikely to happen) an object will be wrapped into a weak reference and stored into a field.
-     */
-    @Volatile
-    private var targetLcgValue: Int = Random.nextInt()
-    private var lcg: Int = Random.nextInt()
-    @Volatile
-    internal var ref: WeakReference<Any>? = null
-
     actual fun consume(obj: Any?) {
-        // https://en.wikipedia.org/wiki/Linear_congruential_generator
-        val next = lcg * 1664525 + 1013904223
-        if ((next and targetLcgValue) == 0) {
-            if (obj !== null) {
-                ref = WeakReference(obj)
-            }
-            targetLcgValue *= 17
-        }
-        lcg = next
+        val hc = obj?.identityHashCode() ?: 0
+        consumeImplULL(hc.convert())
     }
     actual fun consume(bool: Boolean) {
         consumeImplULL(bool.toByte().convert())
@@ -58,6 +36,4 @@ actual class Blackhole {
     }
 }
 
-actual fun Blackhole.flush()  {
-    ref?.clear()
-}
+actual fun Blackhole.flush() = Unit
