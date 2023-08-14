357f69eb04fe6e44651b635ad715131b1a8c5840
wldeh <62161211+wldeh@users.noreply.github.com>
2023-08-14 04:09:55 -0700

    introduce test for outputTimeUnit annoation

--- integration/src/main/kotlin/kotlinx/benchmark/integration/AnnotationsSpecifier.kt
+++ integration/src/main/kotlin/kotlinx/benchmark/integration/AnnotationsSpecifier.kt
@@ -2,9 +2,11 @@ package kotlinx.benchmark.integration
 
 class AnnotationsSpecifier {
     private var isMeasurementSpecified: Boolean = false
+    private var isOutputTimeUnitSpecified: Boolean = false
     private var iterations: Int? = null
     private var time: Int? = null
     private var timeUnit: String? = null
+    private var outputTimeUnit: String? = null
 
     fun measurement(iterations: Int, time: Int, timeUnit: String) {
         isMeasurementSpecified = true
@@ -13,12 +15,19 @@ class AnnotationsSpecifier {
         this.timeUnit = timeUnit
     }
 
+    fun outputTimeUnit(timeUnit: String) {
+        isOutputTimeUnitSpecified = true
+        this.outputTimeUnit = timeUnit
+    }
+
     fun replacementForLine(line: String): String {
         val trimmedLine = line.trimStart()
         val prefix = line.substring(0, line.length - trimmedLine.length)
         return when {
             isMeasurementSpecified && trimmedLine.startsWith("@Measurement") ->
                 "$prefix@Measurement($iterations, $time, $timeUnit)"
+            isOutputTimeUnitSpecified && trimmedLine.startsWith("@OutputTimeUnit") ->
+                "$prefix@OutputTimeUnit($outputTimeUnit)"
             else ->
                 line
         }
--- integration/src/test/kotlin/kotlinx/benchmark/integration/SuiteSourceGeneratorTest.kt
+++ integration/src/test/kotlin/kotlinx/benchmark/integration/SuiteSourceGeneratorTest.kt
@@ -20,4 +20,22 @@ class SuiteSourceGeneratorTest : GradleTest() {
             }
         }
     }
+
+    @Test
+    fun outputTimeUnitAnnotation() {
+        project("kotlin-multiplatform", true).let { runner ->
+            runner.updateAnnotations("src/commonMain/kotlin/CommonBenchmark.kt") {
+                outputTimeUnit("BenchmarkTimeUnit.SECONDS")
+            }
+
+            runner.run("nativeBenchmarkGenerate")
+
+            runner.generatedDir("native", "test/CommonBenchmark_Descriptor.kt") { descriptorFile ->
+                val text = descriptorFile.readText()
+                val parameters = "outputTimeUnit = BenchmarkTimeUnit.SECONDS"
+                    .replace(" ", "\\s+").toRegex()
+                assertTrue(text.contains(parameters), "Parameters: <$parameters> not found in <$text>")
+            }
+        }
+    }    
 }
