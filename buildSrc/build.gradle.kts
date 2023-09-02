c5b2c656370d52af8bdd0bcd010f296172e63ec8
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2023-09-02 15:51:37 +0200

    Advance Gradle version to 8.0.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,14 +1,7 @@
-import org.jetbrains.kotlin.gradle.plugin.*
-import java.util.*
-
 plugins {
     `kotlin-dsl`
 }
 
 repositories {
     mavenCentral()
-}
-
-kotlinDslPluginOptions {
-    experimentalWarning.set(false)
 }
