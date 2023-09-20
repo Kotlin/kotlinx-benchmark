ec4a51a1f9346c8ff7cf400f6e307ffefbecd5b2
wldeh <62161211+wldeh@users.noreply.github.com>
2023-09-19 22:09:49 -0700

    ensure config overrides corresponding values specified with annotations

--- integration/src/test/kotlin/kotlinx/benchmark/integration/ConfigurationTest.kt
+++ integration/src/test/kotlin/kotlinx/benchmark/integration/ConfigurationTest.kt
@@ -1,83 +1,90 @@
 package kotlinx.benchmark.integration
 
 import kotlin.test.*
-import org.gradle.testkit.runner.BuildResult
 
 class ConfigurationTest : GradleTest() {
-
-    private fun testConfiguration(
-        setupBlock: BenchmarkConfiguration.() -> Unit,
-        checkBlock: BuildResult.() -> Unit
-    ) {
-        project("kotlin-multiplatform") {
-            configuration("config") {
-                setupBlock()
-            }
-        }.run("nativeConfigBenchmark") {
-            checkBlock()
-        }
-    }
-
     @Test
     fun testIterationsConfig() {
         val expectedIterations = 2
 
-        testConfiguration(
-            setupBlock = {
+        val runner = project("kotlin-multiplatform") {
+            configuration("iterationsConfig") {
                 iterations = expectedIterations
-            },
-            checkBlock = {
-                val actualIterations = output.lines().count { it.startsWith("Iteration #") }
-                assertEquals(expectedIterations, actualIterations)
             }
-        )
+        }
+
+        runner.updateAnnotations("src/commonMain/kotlin/CommonBenchmark.kt") {
+            measurement(iterations = 5, time = 20, timeUnit = "BenchmarkTimeUnit.NANOSECONDS")
+        }
+
+        runner.run("nativeIterationsConfigBenchmark") {
+            val actualIterations = this.output.lines().count { it.startsWith("Iteration #") }
+            assertEquals(expectedIterations, actualIterations, "Expected $expectedIterations iterations but found $actualIterations")
+        }
     }
 
     @Test
     fun testOutputTimeUnitConfig() {
         val expectedOutputTimeUnit = "ns"
 
-        testConfiguration(
-            setupBlock = {
+        val runner = project("kotlin-multiplatform") {
+            configuration("outputTimeUnitConfig") {
                 outputTimeUnit = expectedOutputTimeUnit
-            },
-            checkBlock = {
-                val actualOutputTimeUnit = output.lines().find { it.contains("ops/$expectedOutputTimeUnit") }
-                assertNotNull(actualOutputTimeUnit, "Expected output to specify time unit as $expectedOutputTimeUnit but was not found.")
             }
-        )
+        }
+
+        runner.updateAnnotations("src/commonMain/kotlin/CommonBenchmark.kt") {
+            measurement(iterations = 1, time = 20, timeUnit = "BenchmarkTimeUnit.NANOSECONDS")
+        }
+
+        runner.run("nativeOutputTimeUnitConfigBenchmark") {
+            val actualOutputTimeUnit = this.output.lines().find { it.contains("ops/$expectedOutputTimeUnit") }
+            assertNotNull(actualOutputTimeUnit, "Expected output to specify time unit as $expectedOutputTimeUnit but was not found.")
+        }
     }
 
     @Test
     fun testWarmUpConfig() {
         val expectedWarmUps = 2
 
-        testConfiguration(
-            setupBlock = {
+        val runner = project("kotlin-multiplatform") {
+            configuration("warmUpConfig") {
                 warmups = expectedWarmUps
-            },
-            checkBlock = {
-                val actualWarmUps = output.lines().count { it.startsWith("Warm-up #") }
-                assertEquals(expectedWarmUps, actualWarmUps, "Expected $expectedWarmUps warm-ups but found $actualWarmUps")
             }
-        )
+        }
+
+        runner.updateAnnotations("src/commonMain/kotlin/CommonBenchmark.kt") {
+            warmup(iterations = 1, time = 1, timeUnit = "BenchmarkTimeUnit.NANOSECONDS")
+        }
+
+        runner.run("nativeWarmUpConfigBenchmark") {
+            val actualWarmUps = this.output.lines().count { it.startsWith("Warm-up #") }
+            assertEquals(expectedWarmUps, actualWarmUps, "Expected $expectedWarmUps warm-ups but found $actualWarmUps")
+        }
     }
 
     @Test
     fun testAverageTimeModeConfig() {
         val expectedMode = "thrpt"
 
-        testConfiguration(
-            setupBlock = {
+        val runner = project("kotlin-multiplatform") {
+            configuration("averageTimeModeConfig") {
                 mode = expectedMode
-            },
-            checkBlock = {
-                val lines = output.lines()
-                val summaryIndex = lines.indexOfFirst { it.contains("native summary:") }
-                val benchmarkLine = lines[summaryIndex + 2]
-                val actualMode = benchmarkLine.split(Regex("\\s+"))[1]
-                assertEquals(expectedMode, actualMode, "Expected mode $expectedMode but found $actualMode")
             }
-        )
+        }
+
+        runner.updateAnnotations("src/commonMain/kotlin/CommonBenchmark.kt") {
+            benchmarkMode("Mode.AverageTime")
+        }
+
+        runner.run("nativeAverageTimeModeConfigBenchmark") {
+            val lines = this.output.lines()
+            val summaryIndex = lines.indexOfFirst { it.contains("native summary:") }
+
+            val benchmarkLine = lines[summaryIndex + 2]
+            val actualMode = benchmarkLine.split(Regex("\\s+"))[1] // specified mode is in second column, two lines down from native summary
+
+            assertEquals(expectedMode, actualMode, "Expected mode $expectedMode but found $actualMode")
+        }
     }
 }
