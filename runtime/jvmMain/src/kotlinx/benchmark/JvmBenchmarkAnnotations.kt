7d122cfe91bcd3d092090e726fb4dbae70121730
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-02-06 18:53:56 +0200

    Fix build failures caused by -Werror

--- runtime/jvmMain/src/kotlinx/benchmark/JvmBenchmarkAnnotations.kt
+++ runtime/jvmMain/src/kotlinx/benchmark/JvmBenchmarkAnnotations.kt
@@ -10,6 +10,8 @@ actual typealias TearDown = org.openjdk.jmh.annotations.TearDown
 
 actual typealias Benchmark = org.openjdk.jmh.annotations.Benchmark
 
+// TODO: This annotation is redundant in K2: KT-59561
+//  K2 currently produces warning for its use.
 @Suppress("NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS")
 actual typealias BenchmarkMode = org.openjdk.jmh.annotations.BenchmarkMode
 
@@ -17,6 +19,8 @@ actual typealias Mode = org.openjdk.jmh.annotations.Mode
 
 actual typealias OutputTimeUnit = org.openjdk.jmh.annotations.OutputTimeUnit
 
+// TODO: This annotation is redundant in K2: KT-59561
+//  K2 currently produces warning for its use.
 @Suppress("ACTUAL_WITHOUT_EXPECT")
 actual typealias BenchmarkTimeUnit = java.util.concurrent.TimeUnit
 
@@ -25,5 +29,7 @@ actual typealias Warmup = org.openjdk.jmh.annotations.Warmup
 @Suppress("ACTUAL_ANNOTATION_CONFLICTING_DEFAULT_ARGUMENT_VALUE")
 actual typealias Measurement = org.openjdk.jmh.annotations.Measurement
 
+// TODO: This annotation is redundant in K2: KT-59561
+//  K2 currently produces warning for its use.
 @Suppress("NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS")
 actual typealias Param = org.openjdk.jmh.annotations.Param
