e8f559adfccc5448e34d6e9d638e1bab016b51ef
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-05-04 14:33:48 +0300

    Create a separate source set for the shared Blackhole
    The CommonBlackhole class is going to be hidden/internalized. Once it is internal, the public Js and WasmJs Blackhole classes won’t be able to inherit from it. This separate source set, shared btw Js and WasmJs, ensures that the targets have a shared implementation, allowing the CommonBlackhole class to be properly internalized or dropped.

--- runtime/jsWasmJsSharedMain/src/kotlinx/benchmark/CommonBlackhole.kt
+++ runtime/jsWasmJsSharedMain/src/kotlinx/benchmark/CommonBlackhole.kt
@@ -4,9 +4,7 @@ import kotlinx.benchmark.internal.KotlinxBenchmarkRuntimeInternalApi
 
 private const val MAGIC_SIZE: Int = 13
 
-// TODO: Drop this class once it becomes internal
-@KotlinxBenchmarkRuntimeInternalApi
-open class CommonBlackhole {
+public actual class Blackhole {
     private val arrayOfAny: Array<Any?> = arrayOfNulls(MAGIC_SIZE)
     private var currentAnyPosition: Int = 0
     private fun consumeAny(obj: Any?) {
@@ -21,26 +19,30 @@ open class CommonBlackhole {
         currentIntPosition = if (currentIntPosition == MAGIC_SIZE - 1) 0 else currentIntPosition + 1
     }
 
-    fun flushMe() {
+    internal fun flushMe() {
         val sums = arrayOfAny.sumOf { it.hashCode() } + arrayOfInt.sum()
         println("Consumed blackhole value: $sums")
     }
 
-    fun consume(obj: Any?) = consumeAny(obj)
+    actual fun consume(obj: Any?) = consumeAny(obj)
 
-    fun consume(bool: Boolean) = consumeInt(bool.hashCode())
+    actual fun consume(bool: Boolean) = consumeInt(bool.hashCode())
 
-    fun consume(c: Char) = consumeInt(c.hashCode())
+    actual fun consume(c: Char) = consumeInt(c.hashCode())
 
-    fun consume(b: Byte) = consumeInt(b.hashCode())
+    actual fun consume(b: Byte) = consumeInt(b.hashCode())
 
-    fun consume(s: Short) = consumeInt(s.hashCode())
+    actual fun consume(s: Short) = consumeInt(s.hashCode())
 
-    fun consume(i: Int) = consumeInt(i.hashCode())
+    actual fun consume(i: Int) = consumeInt(i.hashCode())
 
-    fun consume(l: Long) = consumeInt(l.hashCode())
+    actual fun consume(l: Long) = consumeInt(l.hashCode())
 
-    fun consume(f: Float) = consumeInt(f.hashCode())
+    actual fun consume(f: Float) = consumeInt(f.hashCode())
 
-    fun consume(d: Double) = consumeInt(d.hashCode())
+    actual fun consume(d: Double) = consumeInt(d.hashCode())
 }
+
+
+@KotlinxBenchmarkRuntimeInternalApi
+actual fun Blackhole.flush() = flushMe()
--- runtime/wasmJsMain/src/kotlinx/benchmark/WasmBlackhole.kt
+++
@@ -1,8 +0,0 @@
-package kotlinx.benchmark
-
-import kotlinx.benchmark.internal.KotlinxBenchmarkRuntimeInternalApi
-
-actual class Blackhole : CommonBlackhole()
-
-@KotlinxBenchmarkRuntimeInternalApi
-actual fun Blackhole.flush() = flushMe()
