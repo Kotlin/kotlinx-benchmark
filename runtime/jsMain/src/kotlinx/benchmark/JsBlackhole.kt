a3a38499f8442f3444d4c4bb304944285421ee0f
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-05-04 14:33:48 +0300

    Create a separate source set for the shared Blackhole
    The CommonBlackhole class is going to be hidden/internalized. Once it is internal, the public Js and WasmJs Blackhole classes won’t be able to inherit from it. This separate source set, shared btw Js and WasmJs, ensures that the targets have a shared implementation, allowing the CommonBlackhole class to be properly internalized or dropped.

--- runtime/jsMain/src/kotlinx/benchmark/JsBlackhole.kt
+++
@@ -1,9 +0,0 @@
-package kotlinx.benchmark
-
-import kotlinx.benchmark.internal.KotlinxBenchmarkRuntimeInternalApi
-
-@OptIn(KotlinxBenchmarkRuntimeInternalApi::class)
-actual class Blackhole : CommonBlackhole()
-
-@KotlinxBenchmarkRuntimeInternalApi
-actual fun Blackhole.flush() = flushMe()
