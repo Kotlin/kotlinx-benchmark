29696be315415b5a9d914eed3e8f57a55f5262c9
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-05-06 12:53:13 +0300

    Add tests for SuiteSourceGenerator

--- integration/src/test/kotlin/kotlinx/benchmark/integration/SuiteSourceGeneratorTest.kt
+++ integration/src/test/kotlin/kotlinx/benchmark/integration/SuiteSourceGeneratorTest.kt
@@ -3,22 +3,153 @@ package kotlinx.benchmark.integration
 import kotlin.test.*
 
 class SuiteSourceGeneratorTest : GradleTest() {
+    private fun Runner.assertGeneratedDescriptorContainsCode(code: String) {
+        val regex = code.replace(" ", "\\s*").toRegex(RegexOption.DOT_MATCHES_ALL)
+        generatedDir("native", "test/CommonBenchmark_Descriptor.kt") { descriptorFile ->
+            val text = descriptorFile.readText()
+            assertTrue(text.contains(regex), "Regex: <$regex> not found in <$text>")
+        }
+    }
+
+    private inline fun testSourceGenerator(setupBlock: Runner.() -> Unit, checkBlock: Runner.() -> Unit) {
+        project("source-generation").apply {
+            setupBlock()
+            run("nativeBenchmarkGenerate")
+            checkBlock()
+        }
+    }
+
     @Test
     fun measurementAnnotation() {
-        project("kotlin-multiplatform", true).let { runner ->
-            runner.updateAnnotations("src/commonMain/kotlin/CommonBenchmark.kt") {
-                measurement(iterations = 5, time = 200, timeUnit = "BenchmarkTimeUnit.MILLISECONDS")
+        testSourceGenerator(
+            setupBlock = {
+                updateAnnotations("src/commonMain/kotlin/CommonBenchmark.kt") {
+                    measurement(iterations = 12, time = 200, timeUnit = "BenchmarkTimeUnit.MILLISECONDS")
+                }
+            },
+            checkBlock = {
+                val parameters = "iterations = 12, warmups = 5, iterationTime = IterationTime\\(200, BenchmarkTimeUnit.MILLISECONDS\\)"
+                assertGeneratedDescriptorContainsCode(parameters)
             }
+        )
+    }
 
-            runner.run("nativeBenchmarkGenerate")
-
-            runner.generatedDir("native", "test/CommonBenchmark_Descriptor.kt") { descriptorFile ->
-                val text = descriptorFile.readText()
-                val parameters = "iterations = 5, iterationTime = IterationTime\\(200, BenchmarkTimeUnit.MILLISECONDS\\)"
-                    .replace(" ", "\\s+").toRegex()
-                assertTrue(text.contains(parameters), "Parameters: <$parameters> not found in <$text>")
+    @Test
+    fun warmupAnnotation() {
+        testSourceGenerator(
+            setupBlock = {
+                updateAnnotations("src/commonMain/kotlin/CommonBenchmark.kt") {
+                    warmup(iterations = 12, time = 200, timeUnit = "BenchmarkTimeUnit.MILLISECONDS")
+                }
+            },
+            checkBlock = {
+                // time and timeUnit of @Warmup are ignored: https://github.com/Kotlin/kotlinx-benchmark/issues/74
+                val parameters = "iterations = 3, warmups = 12, iterationTime = IterationTime\\(1, BenchmarkTimeUnit.SECONDS\\)"
+                assertGeneratedDescriptorContainsCode(parameters)
             }
-        }
+        )
+    }
+
+    @Test
+    fun outputTimeUnitAnnotation() {
+        testSourceGenerator(
+            setupBlock = {
+                updateAnnotations("src/commonMain/kotlin/CommonBenchmark.kt") {
+                    outputTimeUnit("BenchmarkTimeUnit.SECONDS")
+                }
+            },
+            checkBlock = {
+                val parameters = "outputTimeUnit = BenchmarkTimeUnit.SECONDS"
+                assertGeneratedDescriptorContainsCode(parameters)
+            }
+        )
+    }
+
+    @Test
+    fun benchmarkModeAnnotation() {
+        testSourceGenerator(
+            setupBlock = {
+                updateAnnotations("src/commonMain/kotlin/CommonBenchmark.kt") {
+                    benchmarkMode("Mode.AverageTime")
+                }
+            },
+            checkBlock = {
+                val parameters = "mode = Mode.AverageTime"
+                assertGeneratedDescriptorContainsCode(parameters)
+            }
+        )
+    }
+
+    @Test
+    fun setupAnnotation() {
+        testSourceGenerator(
+            setupBlock = {
+                addAnnotation("src/commonMain/kotlin/CommonBenchmark.kt") {
+                    setup("function1")
+                    setup("function2")
+                }
+            },
+            checkBlock = {
+                val functionCalls = """((instance.function1\(\).*instance.function2\(\))|(instance.function2\(\).*instance.function1\(\)))"""
+                val regex = "private fun setUp\\(instance: CommonBenchmark\\) \\{ $functionCalls }"
+                assertGeneratedDescriptorContainsCode(regex)
+            }
+        )
+    }
+
+    @Test
+    fun benchmarkAnnotation() {
+        testSourceGenerator(
+            setupBlock = {
+                addAnnotation("src/commonMain/kotlin/CommonBenchmark.kt") {
+                    benchmark("function1")
+                    benchmark("function2")
+                }
+            },
+            checkBlock = {
+                val function1Ref = """CommonBenchmark::function1"""
+                val function2Ref = """CommonBenchmark::function2"""
+                assertGeneratedDescriptorContainsCode(function1Ref)
+                assertGeneratedDescriptorContainsCode(function2Ref)
+            }
+        )
+    }
+
+    @Test
+    fun teardownAnnotation() {
+        testSourceGenerator(
+            setupBlock = {
+                addAnnotation("src/commonMain/kotlin/CommonBenchmark.kt") {
+                    teardown("function1")
+                    teardown("function2")
+                }
+            },
+            checkBlock = {
+                val functionCalls = """((instance.function1\(\).*instance.function2\(\))|(instance.function2\(\).*instance.function1\(\)))"""
+                val regex = "private fun tearDown\\(instance: CommonBenchmark\\) \\{ $functionCalls } "
+                assertGeneratedDescriptorContainsCode(regex)
+            }
+        )
+    }
+
+    @Test
+    fun paramFieldAnnotation() {
+        testSourceGenerator(
+            setupBlock = {
+                addAnnotation("src/commonMain/kotlin/CommonBenchmark.kt") {
+                    param("data1", "1", "2")
+                    param("data2", "a", "b")
+                }
+            },
+            checkBlock = {
+                val parameterList = "parameters = listOf\\(\"data1\", \"data2\"\\)"
+                val data1Values = "\"data1\" to listOf\\(\"\"\"1\"\"\", \"\"\"2\"\"\"\\)"
+                val data2Values = "\"data2\" to listOf\\(\"\"\"a\"\"\", \"\"\"b\"\"\"\\)"
+                assertGeneratedDescriptorContainsCode(parameterList)
+                assertGeneratedDescriptorContainsCode(data1Values)
+                assertGeneratedDescriptorContainsCode(data2Values)
+            }
+        )
     }
 
     @Test
