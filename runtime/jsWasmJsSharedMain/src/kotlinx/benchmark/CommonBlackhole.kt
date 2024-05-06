98e78e4cde25bcb24f0090973b1c5289ddc00001
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-05-06 23:46:43 +0300

    Make Blackhole.consume() functions inline

--- runtime/jsWasmJsSharedMain/src/kotlinx/benchmark/CommonBlackhole.kt
+++ runtime/jsWasmJsSharedMain/src/kotlinx/benchmark/CommonBlackhole.kt
@@ -4,17 +4,22 @@ import kotlinx.benchmark.internal.KotlinxBenchmarkRuntimeInternalApi
 
 private const val MAGIC_SIZE: Int = 13
 
+@Suppress("NOTHING_TO_INLINE")
 public actual class Blackhole {
     private val arrayOfAny: Array<Any?> = arrayOfNulls(MAGIC_SIZE)
     private var currentAnyPosition: Int = 0
-    private fun consumeAny(obj: Any?) {
+
+    @PublishedApi
+    internal fun consumeAny(obj: Any?) {
         arrayOfAny[currentAnyPosition] = obj
         currentAnyPosition = if (currentAnyPosition == MAGIC_SIZE - 1) 0 else currentAnyPosition + 1
     }
 
     private val arrayOfInt: IntArray = IntArray(MAGIC_SIZE)
     private var currentIntPosition: Int = 0
-    private fun consumeInt(i: Int) {
+
+    @PublishedApi
+    internal fun consumeInt(i: Int) {
         arrayOfInt[currentIntPosition] = i
         currentIntPosition = if (currentIntPosition == MAGIC_SIZE - 1) 0 else currentIntPosition + 1
     }
@@ -24,23 +29,23 @@ public actual class Blackhole {
         println("Consumed blackhole value: $sums")
     }
 
-    actual fun consume(obj: Any?) = consumeAny(obj)
+    actual inline fun consume(obj: Any?) = consumeAny(obj)
 
-    actual fun consume(bool: Boolean) = consumeInt(bool.hashCode())
+    actual inline fun consume(bool: Boolean) = consumeInt(bool.hashCode())
 
-    actual fun consume(c: Char) = consumeInt(c.hashCode())
+    actual inline fun consume(c: Char) = consumeInt(c.hashCode())
 
-    actual fun consume(b: Byte) = consumeInt(b.hashCode())
+    actual inline fun consume(b: Byte) = consumeInt(b.hashCode())
 
-    actual fun consume(s: Short) = consumeInt(s.hashCode())
+    actual inline fun consume(s: Short) = consumeInt(s.hashCode())
 
-    actual fun consume(i: Int) = consumeInt(i.hashCode())
+    actual inline fun consume(i: Int) = consumeInt(i.hashCode())
 
-    actual fun consume(l: Long) = consumeInt(l.hashCode())
+    actual inline fun consume(l: Long) = consumeInt(l.hashCode())
 
-    actual fun consume(f: Float) = consumeInt(f.hashCode())
+    actual inline fun consume(f: Float) = consumeInt(f.hashCode())
 
-    actual fun consume(d: Double) = consumeInt(d.hashCode())
+    actual inline fun consume(d: Double) = consumeInt(d.hashCode())
 }
 
 
