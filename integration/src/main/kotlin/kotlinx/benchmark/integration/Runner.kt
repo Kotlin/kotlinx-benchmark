1aefc62c585362e2dca2f7b695519e4d7c5052cf
Adam <152864218+adam-enko@users.noreply.github.com>
2024-06-26 15:38:08 +0200

    update Gradle to 8.7 (#203)
    * update Gradle to 8.7
    
    - replace deprecated Project.buildDir with layout.buildDirectory
    - replace deprecated createTempFile with Files.createTempFile
    - change NativeBenchmarkExec from `open` to `abstract` to avoid `does not implement abstract base class member public abstract fun acceptServiceReferences` error
    * fix default Gradle version in Runner

--- integration/src/main/kotlin/kotlinx/benchmark/integration/Runner.kt
+++ integration/src/main/kotlin/kotlinx/benchmark/integration/Runner.kt
@@ -8,13 +8,16 @@ import java.io.File
 class Runner(
     private val projectDir: File,
     private val print: Boolean,
-    private val gradleVersion: GradleTestVersion? = null,
+    gradleVersion: GradleTestVersion? = null,
 ) {
+    /** Defaults to the minimum Gradle version specified in [kotlinx.benchmark.gradle.BenchmarksPlugin] */
+    private val gradleVersion: GradleTestVersion = gradleVersion ?: MinSupportedGradleVersion
+
     private fun gradle(vararg tasks: String): GradleRunner =
         GradleRunner.create()
             .withProjectDir(projectDir)
             .withArguments(*(defaultArguments() + tasks))
-            .withGradleVersion((gradleVersion ?: MinSupportedGradleVersion).versionString)
+            .withGradleVersion(gradleVersion.versionString)
             .run {
                 if (print) forwardStdOutput(System.out.bufferedWriter()) else this
             }
