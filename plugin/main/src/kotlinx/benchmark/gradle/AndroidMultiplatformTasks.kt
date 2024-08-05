3732fc313c486eb39b2f3198d9c64a860e870f89
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-08-05 13:38:15 +0500

    Change the paths to the kotlin-qualification-task
    To make it work in my laptop

--- plugin/main/src/kotlinx/benchmark/gradle/AndroidMultiplatformTasks.kt
+++ plugin/main/src/kotlinx/benchmark/gradle/AndroidMultiplatformTasks.kt
@@ -2,6 +2,7 @@ package kotlinx.benchmark.gradle
 
 import kotlinx.benchmark.gradle.internal.KotlinxBenchmarkPluginInternalApi
 import org.gradle.api.*
+import org.gradle.api.tasks.TaskProvider
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJvmAndroidCompilation
 import java.io.InputStream
 import java.util.*
@@ -14,7 +15,7 @@ fun Project.processAndroidCompilation(target: KotlinJvmAndroidCompilation) {
     println("processAndroidCompilation: ${target.name}")
     val compilation = target.target.compilations.names.let(::println)
 
-    tasks.register("processAndroid${target.name.capitalize(Locale.getDefault())}Compilation", DefaultTask::class.java) {
+    val generateSourcesTask = tasks.register("processAndroid${target.name.capitalize(Locale.getDefault())}Compilation", DefaultTask::class.java) {
         it.group = "benchmark"
         it.description = "Processes the Android compilation '${target.name}' for benchmarks"
         it.dependsOn("bundle${target.name.capitalize(Locale.getDefault())}Aar")
@@ -22,10 +23,10 @@ fun Project.processAndroidCompilation(target: KotlinJvmAndroidCompilation) {
             unpackAndProcessAar(target) { classDescriptors ->
                 generateBenchmarkSourceFiles(classDescriptors)
             }
-            detectAndroidDevice()
-            createAndroidBenchmarkExecTask()
         }
     }
+
+    createAndroidBenchmarkExecTask(target, generateSourcesTask)
 }
 
 fun Project.detectAndroidDevice() {
@@ -47,38 +48,47 @@ fun Project.detectAndroidDevice() {
 
 
 // Use shell command to execute separate project gradle task
-fun Project.createAndroidBenchmarkExecTask() {
-    // TODO: Project path needs to execute benchmark task
-    val executeBenchmarkPath = "E:/Android/AndroidProjects/kotlin-qualification-task"
-    // Using ./gradlew on Windows shows error:
-    // CreateProcess error=193, %1 is not a valid Win32 application
-    val osName = System.getProperty("os.name").toLowerCase(Locale.ROOT)
-    val gradlewPath = "$executeBenchmarkPath/gradlew" + if (osName.contains("win")) ".bat" else ""
-    val args = listOf("-p", executeBenchmarkPath, "connectedAndroidTest")
+fun Project.createAndroidBenchmarkExecTask(target: KotlinJvmAndroidCompilation, generateSourcesTask: TaskProvider<*>) {
+    tasks.register("android${target.name.capitalize(Locale.getDefault())}Benchmark", DefaultTask::class.java) {
+        it.group = "benchmark"
+        it.description = "Processes the Android compilation '${target.name}' for benchmarks"
+        it.dependsOn(generateSourcesTask)
+        it.doLast {
+            detectAndroidDevice()
 
-    try {
-        println("Running command: $gradlewPath ${args.joinToString(" ")}")
+            // TODO: Project path needs to execute benchmark task
+            val executeBenchmarkPath = "/Users/abduqodiri.qurbonzoda_1/AndroidStudioProjects/kotlin-qualification-task"
+            // Using ./gradlew on Windows shows error:
+            // CreateProcess error=193, %1 is not a valid Win32 application
+            val osName = System.getProperty("os.name").toLowerCase(Locale.ROOT)
+            val gradlewPath = "$executeBenchmarkPath/gradlew" + if (osName.contains("win")) ".bat" else ""
+            val args = listOf("-p", executeBenchmarkPath, "connectedAndroidTest")
 
-        val process = ProcessBuilder(gradlewPath, *args.toTypedArray())
-            .redirectOutput(ProcessBuilder.Redirect.PIPE)
-            .redirectError(ProcessBuilder.Redirect.PIPE)
-            .start()
+            try {
+                println("Running command: $gradlewPath ${args.joinToString(" ")}")
 
-        val outputGobbler = StreamGobbler(process.inputStream) { println(it) }
-        val errorGobbler = StreamGobbler(process.errorStream) { System.err.println(it) }
+                val process = ProcessBuilder(gradlewPath, *args.toTypedArray())
+                    .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                    .redirectError(ProcessBuilder.Redirect.PIPE)
+                    .start()
 
-        outputGobbler.start()
-        errorGobbler.start()
+                val outputGobbler = StreamGobbler(process.inputStream) { println(it) }
+                val errorGobbler = StreamGobbler(process.errorStream) { System.err.println(it) }
 
-        val exitCode = process.waitFor(10, TimeUnit.MINUTES)
-        if (!exitCode || process.exitValue() != 0) {
-            println("Android benchmark task failed with exit code ${process.exitValue()}")
-        } else {
-            println("Benchmark for Android target finished.")
+                outputGobbler.start()
+                errorGobbler.start()
+
+                val exitCode = process.waitFor(10, TimeUnit.MINUTES)
+                if (!exitCode || process.exitValue() != 0) {
+                    println("Android benchmark task failed with exit code ${process.exitValue()}")
+                } else {
+                    println("Benchmark for Android target finished.")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                throw GradleException("Failed to execute benchmark task", e)
+            }
         }
-    } catch (e: Exception) {
-        e.printStackTrace()
-        throw GradleException("Failed to execute benchmark task", e)
     }
 }
 
