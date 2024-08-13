122f7a4b859ee826c109af0105ad4195dddf69bb
Bogdan Mukvich <bogdan.mukvich@jetbrains.com>
2024-08-13 13:48:19 +0200

    Show TeamCity build status in Kotlinx Benchmark GitHub PRs
    ^KTI-1878

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.*
 
