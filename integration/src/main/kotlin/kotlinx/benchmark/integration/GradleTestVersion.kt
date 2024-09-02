36fc3479e361d8276da9000668479fd97f7223c0
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-09-02 17:34:59 +0300

    Verify the minimum supported Kotlin version
    - store the minimal supported Gradle version in libs.versions.toml
    - check that the README is up-to-date in the CheckReadmeTask
    - add integration tests for checking the supported Kotlin version, and logged warning

--- integration/src/main/kotlin/kotlinx/benchmark/integration/GradleTestVersion.kt
+++ integration/src/main/kotlin/kotlinx/benchmark/integration/GradleTestVersion.kt
@@ -4,4 +4,6 @@ enum class GradleTestVersion(val versionString: String) {
     v8_0("8.0.2"),
     MinSupportedGradleVersion("7.4"),
     UnsupportedGradleVersion("7.3"),
+    MinSupportedKotlinVersion("2.0.0"),
+    UnsupportedKotlinVersion("1.9.20"),
 }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -123,6 +123,9 @@ val generatePluginConstants by tasks.registering {
     val minSupportedGradleVersion = libs.versions.minSupportedGradle
     inputs.property("minSupportedGradleVersion", minSupportedGradleVersion)
 
+    val minSupportedKotlinVersion = libs.versions.minSupportedKotlin
+    inputs.property("minSupportedKotlinVersion", minSupportedKotlinVersion)
+
     val kotlinCompilerVersion = libs.versions.kotlin
     inputs.property("kotlinCompilerVersion", kotlinCompilerVersion)
 
@@ -133,6 +136,7 @@ val generatePluginConstants by tasks.registering {
                 |internal object BenchmarksPluginConstants {
                 |  const val BENCHMARK_PLUGIN_VERSION = "${benchmarkPluginVersion.get()}"
                 |  const val MIN_SUPPORTED_GRADLE_VERSION = "${minSupportedGradleVersion.get()}"
+                |  const val MIN_SUPPORTED_KOTLIN_VERSION = "${minSupportedKotlinVersion.get()}"
                 |  const val DEFAULT_KOTLIN_COMPILER_VERSION = "${kotlinCompilerVersion.get()}"
                 |}
                 |""".trimMargin()
