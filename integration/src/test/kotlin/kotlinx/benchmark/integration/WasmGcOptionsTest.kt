d414e0214fb3fc433c5c7ad333f1ffe1a8c3d41f
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-09-02 18:32:57 +0300

    An attempt to fix WasmGcOptionsTest

--- integration/src/test/kotlin/kotlinx/benchmark/integration/WasmGcOptionsTest.kt
+++ integration/src/test/kotlin/kotlinx/benchmark/integration/WasmGcOptionsTest.kt
@@ -2,23 +2,29 @@ package kotlinx.benchmark.integration
 
 import org.junit.Test
 
+// TODO: Remove this test and the associated code that adds the "--experimental-wasm-gc" flag once
+//  the minimum supported Kotlin version no longer supports NodeJs < 22.0.0 and D8 < 12.3.68.
 class WasmGcOptionsTest : GradleTest() {
     @Test
     fun nodeJs() {
-        // The test uses Kotlin 1.9.24 as previous versions
-        // would append the --experimental-wasm-gc flag causing run failures.
+        // The default NodeJs version used with the current kotlin_version is 22.0.0 (or newer).
+        // Therefore, the test project intentionally uses an older NodeJs version.
         val runner = project(
-            "wasm-gc-non-experimental/wasm-nodejs", true, kotlinVersion = "1.9.24"
+            "wasm-gc-non-experimental/wasm-nodejs",
+            print = true,
+            kotlinVersion = GradleTestVersion.MinSupportedKotlinVersion.versionString
         )
         runner.runAndSucceed("wasmJsBenchmark")
     }
 
     @Test
     fun d8() {
-        // The test uses Kotlin 1.9.24 as previous versions
-        // would append the --experimental-wasm-gc flag causing run failures.
+        // The default D8 version used with the current kotlin_version is 11.9.85 (or newer).
+        // Therefore, the test project intentionally uses a version newer than 12.3.68.
         val runner = project(
-            "wasm-gc-non-experimental/wasm-d8", true, kotlinVersion = "1.9.24"
+            "wasm-gc-non-experimental/wasm-d8",
+            print = true,
+            kotlinVersion = GradleTestVersion.MinSupportedKotlinVersion.versionString
         )
         runner.runAndSucceed( "wasmJsBenchmark")
     }
