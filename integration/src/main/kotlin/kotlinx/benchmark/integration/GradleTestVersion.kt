51d69c51d07a235b4302a88e891eb585eb8df652
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-09-30 23:01:55 +0300

    fixup! WA: set MinSupportedGradleVersion to "7.6.3"

--- integration/src/main/kotlin/kotlinx/benchmark/integration/GradleTestVersion.kt
+++ integration/src/main/kotlin/kotlinx/benchmark/integration/GradleTestVersion.kt
@@ -3,7 +3,7 @@ package kotlinx.benchmark.integration
 enum class GradleTestVersion(val versionString: String) {
     v8_0("8.0.2"),
     MinSupportedGradleVersion("7.6.3"),
-    UnsupportedGradleVersion("7.6.3"),
+    UnsupportedGradleVersion("7.6.2"),
     MinSupportedKotlinVersion("2.0.0"),
     UnsupportedKotlinVersion("1.9.20"),
 }
