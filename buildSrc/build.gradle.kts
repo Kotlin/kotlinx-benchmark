3d280eb995d052f4df80e328ab64e922ef2a3ea2
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-04-22 17:39:58 -0400

    Setup toolchain for buildSrc too

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,13 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     `kotlin-dsl`
+}
+
+kotlin {
+    jvmToolchain(17)
+    compilerOptions.jvmTarget = JvmTarget.JVM_1_8
+}
+tasks.withType(JavaCompile::class) {
+    options.release = 8
 }
