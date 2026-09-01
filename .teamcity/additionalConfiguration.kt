d38223cb5c8b1feb0b55781d2cda2db71b203d34
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-09-01 15:07:03 -0400

    Use higher min Gradle version for builds against Kotlin master
    Follow up for #393

--- .teamcity/additionalConfiguration.kt
+++ .teamcity/additionalConfiguration.kt
@@ -135,7 +135,7 @@ fun Project.buildWithKotlinMaster(platform: Platform, versionBuild: BuildType) =
                 "-P$versionSuffixParameter=%$versionSuffixParameter%", "-P$teamcitySuffixParameter=%$teamcitySuffixParameter%",
                 "-Pkotlin_repo_url=file://%teamcity.build.checkoutDir%/artifacts/kotlin",
                 "-Pkotlin_version=%$kotlinVersionParameter%", "-Pkotlin.native.version=%$kotlinVersionParameter%",
-                "-Pmin_supported_gradle_version=8.0",
+                "-Pmin_supported_gradle_version=8.14.5",
                 "-Pkotlin.native.enableKlibsCrossCompilation=false"
             ).joinToString(separator = " ")
             buildFile = ""
