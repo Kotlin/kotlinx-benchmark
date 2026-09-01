f28fce3fc21224a2ada7108ced526be1de33c585
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-09-01 19:01:57 -0400

    fixup! Update minSupportedGradleVersion to 8.14.5

--- integration/src/main/kotlin/kotlinx/benchmark/integration/GradleTestVersion.kt
+++ integration/src/main/kotlin/kotlinx/benchmark/integration/GradleTestVersion.kt
@@ -1,7 +1,7 @@
 package kotlinx.benchmark.integration
 
 enum class GradleTestVersion(val versionString: String) {
-    v8_14_5("8.14.5"),
+    v8_0("8.0.2"),
     MinSupportedGradleVersion(System.getProperty("minSupportedGradleVersion")),
     UnsupportedGradleVersion("7.3"),
     v9_3_0("9.3.0"),
